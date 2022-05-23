8aa1da3dbf74bcbe9a67b83085101672d951921b
Africo da Silva <joao.africo.da.silva@accenture.com>
2022-05-23 11:59:48 -0300

    update JenkinsFIle

--- src/test/java/br/com/JMAfrico/tasks/functional/TasksTest.java
+++ src/test/java/br/com/JMAfrico/tasks/functional/TasksTest.java
@@ -21,7 +21,7 @@ public class TasksTest {
 	
 	public WebDriver acessarAplicacao() {
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://localhost:8001/tasks");
+		driver.navigate().to("http://localhost:8080/tasks");
 		//wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		return driver;
